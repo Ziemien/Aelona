0046665114537c9c44ec54d31f305bf4c2d01660
Darth Vader <brave.pineapple@gmail.com>
2017-03-19 14:59:15 +0000

    some flames + other flames

---
+++ Assets/Scripts/BridgeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour
+{
+    public GameObject leftSide;
+    public GameObject rightSide;
+
+    public float speed = 0.25f;
+
+    private float state = 0;
+
+    private Quaternion initialStateRight;
+    private Quaternion initialStateLeft;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    initialStateLeft = leftSide.transform.localRotation;
+	    initialStateRight = rightSide.transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var deltaTime = Time.deltaTime;
+	    state = Mathf.Min(deltaTime * speed + state, 1);
+
+	    leftSide.transform.localRotation = Quaternion.Lerp(initialStateLeft, Quaternion.identity, state);
+	    rightSide.transform.localRotation = Quaternion.Lerp(initialStateRight, Quaternion.identity, state);
+	}
+}
