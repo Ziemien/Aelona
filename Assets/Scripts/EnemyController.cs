2a1eb58b8c492977f686fe200e946461dedf5fce
Cameron Edwards <cameronedwards145@gmail.com>
2017-03-19 15:26:54 +0000

    Enemies explode

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,10 +6,18 @@ public class EnemyController : MonoBehaviour
 {
     public CharacterController player;
     public float speed = 1;
+    public float turnSpeed = 90;
+
+    private ParticleSystem explosionParticles;
+    private SpriteRenderer spriteRenderer;
+    public bool isDead;
 
     // Use this for initialization
     void Start()
     {
+        explosionParticles = GetComponentInChildren<ParticleSystem>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         if (player == null)
         {
             player = FindObjectOfType<CharacterController>();
@@ -19,10 +27,36 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = player.transform.position - transform.position;
-        float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        if (isDead)
+        {
+            return;
+        }
 
-        transform.Translate(Vector2.up * Time.deltaTime * speed);
+        Vector2 direction = player.transform.position - transform.position;
+        float targetRotationZ = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + 90;
+
+        float difference = targetRotationZ - transform.rotation.eulerAngles.z;
+        float rotationThisFrame = turnSpeed * Time.deltaTime;
+        if (difference > rotationThisFrame)
+        {
+            transform.Rotate(0, 0, rotationThisFrame);
+        }
+        else
+        {
+            transform.Rotate(0, 0, difference);
+        }
+
+        transform.Translate(Vector2.down * Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            spriteRenderer.enabled = false;
+            explosionParticles.Play();
+            isDead = true;
+        }
     }
 }
